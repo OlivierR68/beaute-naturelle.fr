a762506c1e9f327a4057c33c13abfdfff30c8108
claisse <coldisiitp19-cmo@ccicampus.fr>
2020-01-21 16:11:15 +0100

    prestations

--- 2_Developpement/Mauran/prestations_tarifs_manager.php
+++ 2_Developpement/Mauran/prestations_tarifs_manager.php
@@ -1,7 +1,7 @@
 <?php
 	include("connect_bdd.php");
 	
-	class User_manager extends Connection{
+	class prestations_tarifs_manager extends Connection{
 		
 		public function __construct(){
 			parent::__construct();
@@ -9,11 +9,11 @@
 		
 		public function is_in_base(){
 			//var_dump($_POST);
-			$strQuery = "	SELECT user_id, user_name, user_firstname,
-									user_mail
-							FROM users
-							WHERE user_mail = '".$_POST['mail']."'
-								AND user_pwd = '".$_POST['passwd']."'
+			$strQuery = "	SELECT categorie_id, categorie_name, categorie_firstname,
+									categorie_mail
+							FROM categorie
+							WHERE categorie_mail = '".$_POST['mail']."'
+								AND categorie_pwd = '".$_POST['passwd']."'
 							";
 			/*$query = $this->_db->query($strQuery);
 			return $query->fetchAll();*/
@@ -21,40 +21,40 @@
 		}
 		
 		/*public function is_in_base2($mail, $pwd){
-			$strQuery = "	SELECT user_id, user_name, user_firstname,
-									user_mail
-							FROM users
-							WHERE user_mail = '".$mail."'
-								AND user_pwd = '".$pwd."'
+			$strQuery = "	SELECT categorie_id, categorie_name, categorie_firstname,
+									categorie_mail
+							FROM categorie
+							WHERE categorie_mail = '".$mail."'
+								AND categorie_pwd = '".$pwd."'
 							";
 
 			return $this->_db->query($strQuery)->fetchAll();
 		}*/
 
 		public function get(){
-			$strQuery = "	SELECT user_id, user_name, user_firstname,
-									user_mail
-							FROM users
-							WHERE user_id = ".$_SESSION['user']['id'];
+			$strQuery = "	SELECT categorie_id, categorie_name, categorie_firstname,
+									categorie_mail
+							FROM categorie
+							WHERE categorie_id = ".$_SESSION['categorie']['id'];
 			return $this->_db->query($strQuery)->fetch();
 		}
 
 		public function edit($objUser, $boolEditPwd = false){
-			$strQuery = "	UPDATE users
-							SET user_name = '".$objUser->getName()."',
-								user_firstname = '".$objUser->getFirstname()."',
-								user_mail = '".$objUser->getMail()."'";
+			$strQuery = "	UPDATE categorie
+							SET categorie_name = '".$objUser->getName()."',
+								categorie_firstname = '".$objUser->getFirstname()."',
+								categorie_mail = '".$objUser->getMail()."'";
 			if ($boolEditPwd){
-				$strQuery .= "	, user_pwd = '".$objUser->getPwd()."'";
+				$strQuery .= "	, categorie_pwd = '".$objUser->getPwd()."'";
 			}
 								
-			$strQuery .= "	WHERE user_id = ".$_SESSION['user']['id'];
+			$strQuery .= "	WHERE categorie_id = ".$_SESSION['user']['id'];
 			return $this->_db->exec($strQuery);
 		}
 
 		public function add($objUser){
-			$strQuery = "	INSERT INTO users (user_name, user_firstname, 
-												user_mail, user_pwd)
+			$strQuery = "	INSERT INTO categorie (categorie_name, categorie_firstname, 
+												categorie_mail, categorie_pwd)
 							VALUES ('".$objUser->getName()."', '".$objUser->getFirstname()."', 
 									'".$objUser->getMail()."', '".$objUser->getPwd()."')";
 			return $this->_db->exec($strQuery);
