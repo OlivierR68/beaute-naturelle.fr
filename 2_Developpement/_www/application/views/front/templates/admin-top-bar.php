a362bf8a8cf1290c31a2c1866d49d89df8bb62ee
OlivierR68 <57542189+OlivierR68@users.noreply.github.com>
2020-01-22 08:42:07 +0100

    ajout top bar admin

---
+++ 2_Developpement/_www/application/views/front/templates/admin-top-bar.php
@@ -0,0 +1,57 @@
+<!-- TOP BAR ADMIN -->
+<div class="bg-dark">
+	<nav class="navbar navbar-expand-lg navbar-dark container">
+		<a class="navbar-brand" href="<?php base_url('dashboard/') ?>"><i class="fas fa-cogs mr-2"></i>Administrateur</a>
+		<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a  class="nav-link" href="#"><i class="fas fa-tachometer-alt mr-2"></i>Tableau de bord</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Accès rapide
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Action</a>
+						<a class="dropdown-item" href="#">Another action</a>
+						<a class="dropdown-item" href="#">Something else here</a>
+						<a class="dropdown-item" href="#">Something else here</a>
+						<a class="dropdown-item" href="#">Something else here</a>
+						<a class="dropdown-item" href="#">Something else here</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Disabled</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+</div>
+
+
+<div class="bg-dark bn_adm-top-bar">
+
+
+	<!--
+	<div class="container py-2 d-flex justify-content-between">
+		<div class="">
+			<a href="#"><i class="fas fa-tachometer-alt mr-2"></i>Tableau de bord</a>
+		</div>
+		<div class="">
+			<a href="#"><span class="bn_cube mr-2">10</span></i>Inscriptions<span class="d-none d-sm-inline"> en cours</span></a>
+			<a href="#"><span class="bn_cube mr-2">0</span></i>Images<span class="d-none d-sm-inline"> à valider</span></a>
+		</div>
+
+	</div>
+
+	-->
+</div>
