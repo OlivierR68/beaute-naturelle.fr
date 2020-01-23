593c163404d60ac6e2f382966504b4d6226e08f3
BonsoirTLM68 <marc-chanteranne@outlook.fr>
2020-01-23 16:18:20 +0100

    MaJ Events dynamique

--- 2_Developpement/_www/application/models/Events_manager.php
+++ 2_Developpement/_www/application/models/Events_manager.php
@@ -6,8 +6,7 @@ class Events_manager extends CI_Model {
 
 	public function findAll(){
 		$this->db->select("*");
-        $this->db->from("event, event_user");
-        $this->db->where("event_id.event = event_id.event_user");
+        $this->db->from("event");
 		$this->db->order_by("event_id", "asc");
 
 		$queryGroup	= $this->db->get();
--- 2_Developpement/_www/application/views/front/events.php
+++ 2_Developpement/_www/application/views/front/events.php
@@ -2,29 +2,32 @@
 <main class="container bn_content">
 
 <!-- Foreach ici -->
-	<article>
-		<div class="row">
-			<div class="col-4">
+	<?php foreach($arrEvents as $objEvent){ ?>
+		<article>
+			<div class="row">
+				<div class="col-4">
 
-				<img class="img-fluid" src="<?php echo base_url() ?>/assets/img/events/flacon-de-fleur-orange-huile.jpg" alt="flacon d'huile" srcset="">
+					<img class="img-fluid" src="<?php echo base_url()."assets/img/".$objEvent->getImg() ?>" alt="flacon d'huile" srcset="">
 
-			</div>
-
-			<div class="col-8">
-				<span class="bn_h2-pre">Du 13/02/2020 au 14/02/2020</span>
-				<h2>Exemple nom d'article</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis iste quidem reiciendis! Corporis eaque, eius eum exercitationem fugiat hic, in laborum libero nobis placeat provident rerum sit, tenetur ut.</p>
-				<div>
-					<span class="bn_h2-pre"><i class="fas fa-users"></i> Places disponibles : 1/20</span>
 				</div>
-				<div class="bn_gap-10"></div>
-				<div>
-					<a  href="#" class="btn bn_btn-green text-uppercase">S'INSCRIRE</a>
+
+				
+				<div class="col-8">
+					<span class="bn_h2-pre">Du <?php echo $objEvent->getStart_date() ?> au <?php echo $objEvent->getEnd_date() ?></span>
+					<h2><?php echo $objEvent->getName() ?></h2>
+					<p><?php echo $objEvent->getContent() ?></p>
+					<div>
+						<span class="bn_h2-pre"><i class="fas fa-users"></i> Places disponibles : 1/<?php echo $objEvent->getCapacity() ?></span>
+					</div>
+					<div class="bn_gap-10"></div>
+					<div>
+						<a  href="#" class="btn bn_btn-green text-uppercase">S'INSCRIRE</a>
+					</div>
 				</div>
 			</div>
-		</div>
-	</article>
-	<div class="bn_gap-25"></div>
+		</article>
+		<div class="bn_gap-25"></div>
+	<?php } ?>
 <!-- fin du foreach -->
 
 </main>
