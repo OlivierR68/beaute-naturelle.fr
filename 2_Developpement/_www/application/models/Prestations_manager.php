aebf65fda0a9311366ae3d687382a314cd32fc28
claisse <coldisiitp19-19cmo@ccicampus.fr>
2020-01-27 09:49:46 +0100

    prestation

--- 2_Developpement/_www/application/models/Prestations_manager.php
+++ 2_Developpement/_www/application/models/Prestations_manager.php
@@ -1,1 +1,12 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class prestation_manager extends CI_Model{
+	
+	public function findAll(){
+		$this->db->select("*");
+		$this->db->from("prestation");
+		$this->db->order_by("prestation_id", "asc");
+		return $query->result('array');
+	}
+}
