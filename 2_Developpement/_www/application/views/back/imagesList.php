e5dc47d667aa96108c0b265857d02fe2459f528a
Nevets08 <Nevets08@users.noreply.github.com>
2020-01-31 11:46:56 +0100

    Final dashboard galerie

--- 2_Developpement/_www/application/views/back/imagesList.php
+++ 2_Developpement/_www/application/views/back/imagesList.php
@@ -28,4 +28,30 @@
 					</tr>
 				</tfoot>
 				<tbody>
-				
+				<?php var_dump($arrImages);
+				foreach($arrImages as $objImage) { ?>
+					<tr>
+						<td><?php echo $objImage->getId() ?></td>
+						<td class="bn_action">
+
+
+							<a href="<?php echo base_url('images/addEdit/'.$objImage->getId())  ?>" title="Modifier"><i class="far fa-edit"></i></a> |
+							<a href="<?php echo base_url('images/copy/'.$objImage->getId())  ?>" title="Copier"><i class="far fa-copy"></i></a> |
+
+							<?php if($objImage->getId() == true) { ?>
+								<i title="slide" class="fas fa-trash-alt text-muted"></i>
+							<?php } else { ?>
+								<a href="<?php echo base_url('images/delete/'.$objImages>getId())  ?>" title="Supprimer"><i class="fas fa-trash-alt text-danger"></i></a>
+							<?php } ?>
+
+						</td>
+						<td><?php echo $objImage->getSlug() ?></td>
+						<td><?php echo $objImage->getSrc() ?></td>
+						<td><?php echo $objImage->getDescription() ?></td>
+						<td><?php echo $objImage->getAuthor() ?></td>
+						<td><?php echo $objImage->getPubli_date() ?></td>
+					</tr>
+				<?php }  ?>
+				</tbody>
+			</table>
+		</div>
