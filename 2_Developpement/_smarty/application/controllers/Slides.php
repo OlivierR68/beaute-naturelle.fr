350e893a8620b91403688365ea1b94998bda0c00
OlivierR68 <57542189+OlivierR68@users.noreply.github.com>
2020-01-31 23:01:14 +0100

    Update Slides.php

--- 2_Developpement/_smarty/application/controllers/Slides.php
+++ 2_Developpement/_smarty/application/controllers/Slides.php
@@ -56,60 +56,7 @@ class Slides extends CI_Controller {
 		$data['CONTENT']	= $this->load->view('back/slidesList', $data, TRUE);
 		$this->load->view('back/content', $data);
 	}
-
-	/** Back : Fonction pour afficher la page d'un slide  (ancienne fonction plus utilisé...
-	public function addPage()
-	{
-		$data['TITLE'] 		= "Ajouter un slide";
-
-
-
-
-		if(count($this->input->post())>0){
-
-
-			$config['upload_path']      = './uploads/slider/';
-			$config['allowed_types']    = 'gif|jpg|png';
-			$config['max_size']        	= 2048;
-
-			$this->upload->initialize($config);
-			$this->upload->do_upload('img');
-
-
-			if (!$this->upload->do_upload('img'))
-			{
-				$data['ERROR'] = $this->upload->display_errors();
-
-			} else {
-
-				$_POST['img'] = $_FILES['img']['name'];
-
-
-				$objSlide = new Slide_class();
-				$objSlide->hydrate($this->input->post());
-
-
-				$this->Slides_manager->new($objSlide);
-
-				redirect('slides/ListPage', 'refresh');
-
-			}
-
-
-		}
-
-		$_POST = array();
-		$data['CONTENT']	= $this->load->view('back/slidesAdd', $data, TRUE);
-		$this->load->view('back/content', $data);
-
-	}
-
-
-	 
-	 */
-
-
-	/** Back : Fonction d'éditer un slide */
+	
 
 
 	/**
