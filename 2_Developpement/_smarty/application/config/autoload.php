282f5a82893e94021a56cb8de3be7ebd13562bd7
OlivierR68 <57542189+OlivierR68@users.noreply.github.com>
2020-01-31 19:17:07 +0100

    base code igniter

--- 2_Developpement/_smarty/application/config/autoload.php
+++ 2_Developpement/_smarty/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('smartie' => 'smarty');
+$autoload['libraries'] = array('SmartyLibrary' => 'smarty');
 
 /*
 | -------------------------------------------------------------------
--- 2_Developpement/_smarty/application/language/english/label_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['username']  = "User Name";
-$lang['password']  = "Password";
-$lang['passconf']  = "Confirm";
-$lang['email']     = "Email";
-$lang['state']     = "State";
